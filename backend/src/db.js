e93bfc83b4b087c59411810474dc322744216185
300jay <jaycd.cricklover@gmail.com>
2026-04-24 16:02:40 +0530

    bug fixes to db

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,20 +1,14 @@
 const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQLDATABASE
+const db = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQLDATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
-(async () => {
-    try {
-        const conn = await db.getConnection();
-        console.log("✅ Connected to MySQL");
-        conn.release();
-    } catch (err) {
-        console.error("❌ MySQL Connection Failed:", err.message);
-    }
-})();
